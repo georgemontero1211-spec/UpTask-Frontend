f452347b56fb5ffb25ee923937cf4ace9e4ca5cb
georegmontero1211@gmail.com <georgemontero1211@gmail.com>
2025-10-21 17:02:21 -0400

    Initial commit

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import DashBoardView from "./views/DashBoardView";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<DashBoardView />} index />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
