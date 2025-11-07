28ff2af369e488e83c9bb4db07fbcf716d272d26
georegmontero1211@gmail.com <georgemontero1211@gmail.com>
2025-11-07 00:29:21 -0400

    Agregue la seccion para ver los detalles de los proyectos

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Logo from "@/components/Logo";
 import NavMenu from "@/components/NavMenu";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function AppLayout() {
   return (
@@ -10,7 +10,9 @@ export default function AppLayout() {
       <header className="bg-gray-800 p-5">
         <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
           <div className="w-64">
-            <Logo />
+            <Link to="/">
+              <Logo />
+            </Link>
           </div>
           <NavMenu />
         </div>
