8abe94da58e564b7e2a5185fe856eb4350c72907
George Montero Núñez <georgemontero1211@gmail.com>
2025-12-23 20:03:14 -0400

    Ya se visualizan en el DOM los colaboradores de un proyecto

--- src/services/TeamServices.ts
+++ src/services/TeamServices.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import type { Project, TeamMember, TeamMemberForm } from "@/types";
+import { teamMembersSchema, type Project, type TeamMember, type TeamMemberForm } from "@/types";
 import { isAxiosError } from "axios";
 
 export async function findUserByEmal({
@@ -30,9 +30,24 @@ export async function addUserToProject({
 }) {
   try {
     const url = `/projects/${projectId}/team`;
-    const { data } = await api.post(url, {id});
+    const { data } = await api.post(url, { id });
     console.log(data);
     return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getProjectTeam(projectId: Project["_id"]) {
+  try {
+    const url = `/projects/${projectId}/team`;
+    const { data } = await api(url);
+    const response = teamMembersSchema.safeParse(data)
+    if(response.success){
+      return response.data
+    }
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
